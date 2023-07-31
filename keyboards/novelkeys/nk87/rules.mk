2e8ffae6431912d2c1f5185b5509343389b95e64
Samuel Lewis <mazuel@gmail.com>
2023-07-31 10:08:58 +1000

    Added rules config for tesmart kvm

--- keyboards/novelkeys/nk87/rules.mk
+++ keyboards/novelkeys/nk87/rules.mk
@@ -11,7 +11,7 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
+# NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
@@ -29,3 +29,9 @@ SRC =	keyboards/wilba_tech/wt_main.c \
 # Custom build options
 COMBO_ENABLE = no           # https://docs.qmk.fm/#/feature_combo
 TAP_DANCE_ENABLE = no       # https://docs.qmk.fm/#/feature_tap_dance
+
+# https://github.com/qmk/qmk_firmware/issues/19472#issuecomment-1368810027
+# For use with KVM
+KEYBOARD_SHARED_EP = no
+NKRO_ENABLE = no
+VIA_ENABLE = no
